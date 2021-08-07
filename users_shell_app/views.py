427af9c911ebe749d0e922198d79b9f83ef3daea
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2021-08-06 20:31:37 -0600

    First Commit

---
+++ users_shell_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User
+
+def index(request):
+    context = {
+    "all_the_users": User.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def submit(request):
+    if request.method == "POST":
+        User.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email_address = request.POST['email'], age = request.POST['age'])
+    return redirect('/')

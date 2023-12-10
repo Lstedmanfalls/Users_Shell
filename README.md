6d577f7ff6072cde95bbe289f683ccd9133347be
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2023-12-09 18:54:53 -0700

    refactor some

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1> Boilerplate Django App for adding users to a database </h1>
+
+1) First run `python manage.py migrate` to spin up a sqlite database with the project's tables
+2) Next, run the app locally with `python manage.py runserver`
+3) Go to 127.0.0.1:8000 to interact with the application UI
--- users_shell/settings.py
+++ users_shell/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '547_757wu-bch8zk%8uwxz$6g87-4+u_ns__y*$g(5(gw)z8a0'
+SECRET_KEY = 'none'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
